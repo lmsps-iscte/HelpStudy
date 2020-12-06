4b2b03c79450b256686e85d2933471d2dd621622
lmsps-iscte <lmsps@iscte-iul.pt>
2020-12-06 19:30:12 +0000

    Schedule done.

--- src/ScheduleController.scala
+++ src/ScheduleController.scala
@@ -1,10 +1,11 @@
 import java.net.URL
-import java.time.LocalDate
+import java.time.{LocalDate, LocalTime}
 import java.time.format.DateTimeFormatter
 import java.util.ResourceBundle
 
+import javafx.collections.FXCollections
 import javafx.fxml.{FXML, Initializable}
-import javafx.scene.control.{Label, ListView}
+import javafx.scene.control.{Button, Label, ListView, TextField}
 
 class ScheduleController extends Initializable {
 
@@ -29,9 +30,16 @@ class ScheduleController extends Initializable {
     @FXML private var date5: Label = _
     @FXML private var date6: Label = _
     @FXML private var date7: Label = _
-
-
-
+    @FXML private var ratioTextBox: TextField = _
+    @FXML private var dateTextField: TextField = _
+    @FXML private var sTimeTextField: TextField = _
+    @FXML private var eTimeTextField: TextField = _
+    @FXML private var titleTextField: TextField = _
+    @FXML private var cUnitTextField: TextField = _
+    @FXML private var addButton: Button = _
+    @FXML private var editButton: Button = _
+    @FXML private var deleteButton: Button = _
+    @FXML private var updateButton: Button = _
 
     def initialize(location: URL, resources: ResourceBundle): Unit = {
 
@@ -52,17 +60,54 @@ class ScheduleController extends Initializable {
       dWeek7.setText(LocalDate.now().plusDays(6).getDayOfWeek.toString)
 
 
-      /*val note1: Note = ("Nota1","Corpo1","MC")
-      val note2: Note = ("Nota2","Corpo2","TS")
-      val note3: Note = ("Nota3","Corpo3","PPM")
-      val notebook = Notebook(List())
-      val notebook1 = notebook.addNote(note1)
-      val notebook2 = notebook1.addNote(note2)
-      val notebook3 = notebook2.addNote(note3)
+      val bloco1 = SBlock(LocalDate.parse("06-12-2020", DateTimeFormatter.ofPattern("dd-MM-yyyy")),
+        LocalTime.of(9,30,0), LocalTime.of(10,30,0), "Aula TP de MC", "MC")
+      val bloco2 = SBlock(LocalDate.parse("07-12-2020", DateTimeFormatter.ofPattern("dd-MM-yyyy")),
+        LocalTime.of(10,30,0), LocalTime.of(11,30,0), "Aula TP de MC", "CDSI")
+      val bloco3 = SBlock(LocalDate.parse("08-12-2020", DateTimeFormatter.ofPattern("dd-MM-yyyy")),
+        LocalTime.of(11,30,0), LocalTime.of(13,0,0), "Aula TP de MC", "MC")
+      val schedule = Schedule(List(),50)
+      val schedule1 = schedule.addSBlock(bloco1)
+      val schedule2 = schedule1.addSBlock(bloco2)
+      val schedule3 = schedule2.addSBlock(bloco3)
+      ratioTextBox.setText(schedule3.school_percent.toString)
+      var list_obs = FXCollections.observableArrayList[SBlock]()
+      schedule3.sblocks.forall(list_obs.add(_))
+      listView1.setItems(list_obs)
+    }
 
-      var list_obs = FXCollections.observableArrayList[Note]()
-      notebook3.notes.forall(list_obs.add(_))
-      notesListView.setItems(list_obs)*/
+    def addFunc(): Unit = {
+      val date = LocalDate.parse(dateTextField.getText().trim)
+      val stime = LocalTime.parse(sTimeTextField.getText().trim)
+      val etime = LocalTime.parse(eTimeTextField.getText().trim)
+      val title = titleTextField.getText.trim
+      val cunit = cUnitTextField.getText().trim
+      val sblock = SBlock(date, stime, etime, title, cunit)
+      listView1.getItems.add(sblock)
+      dateTextField.clear()
+      sTimeTextField.clear()
+      eTimeTextField.clear()
+      titleTextField.clear()
+      cUnitTextField.clear()
+    }
+
+    def editFunc(): Unit = {
+      val item = listView1.getSelectionModel.getSelectedItem
+      dateTextField.setText(item.date.toString)
+      sTimeTextField.setText(item.start_time.toString)
+      eTimeTextField.setText(item.end_time.toString)
+      titleTextField.setText(item.title)
+      cUnitTextField.setText(item.cunit)
+      var list = listView1.getItems.remove(item)
+   }
+
+    def deleteFunc(): Unit = {
+      val item = listView1.getSelectionModel.getSelectedItem
+      var list = listView1.getItems.remove(item)
+    }
+
+    def updateFunc(): Unit = {
+
     }
 
 }
